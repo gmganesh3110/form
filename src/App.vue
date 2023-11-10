343a005139c46f589edb2c677b00dd398b6f3ca9
G M Ganesh <gmganesh173@gmail.com>
2023-11-10 10:07:01 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <form @submit.prevent="handleSubmit">
+    <div v-for="block in blocks" :key="block.token">
+      <label :for="block.token">{{ block.props.title }}</label>
+        <input
+          :type="block.type"
+          :placeholder="block.props.placeholder"
+          v-model="formData[block.token]"
+          :required="typeof block.props.required === 'string' ? formData[block.props.required] : block.props.required"
+        />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
+import { ref } from 'vue';
 
-nav {
-  padding: 30px;
-}
+const blocks = ref([
+  {
+    token: 'PERSON_NAME',
+    type: 'text',
+    props: {
+      title: 'Enter your name',
+      required: true,
+      placeholder: 'e.g John Doe',
+    },
+  },
+  {
+    token: 'IS_PERSON_MINOR',
+    type: 'checkbox',
+    props: {
+      title: 'Is the current person a minor',
+      default: false,
+    },
+  },
+  {
+    token: 'PERSON_DOB',
+    type: 'date',
+    props: {
+      title: 'Enter your DOB',
+      placeholder: 'e.g 01/01/2000',
+      required:"IS_PERSON_MINOR"
+    },
+  },
+]);
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+const formData = ref({});
+const handleSubmit = () => {
+  console.log('Form submitted:', formData.value);
+  resetForm();
+};
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+const resetForm = () => {
+  formData.value = {};
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="handleSubmit">
+    <div v-for="block in blocks" :key="block.token">
+      <label :for="block.token">{{ block.props.title }}</label>
+        <input
+          :type="block.type"
+          :placeholder="block.props.placeholder"
+          v-model="formData[block.token]"
+          :required="typeof block.props.required === 'string' ? formData[block.props.required] : block.props.required"
+        />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+const blocks = ref([
+  {
+    token: 'PERSON_NAME',
+    type: 'text',
+    props: {
+      title: 'Enter your name',
+      required: true,
+      placeholder: 'e.g John Doe',
+    },
+  },
+  {
+    token: 'IS_PERSON_MINOR',
+    type: 'checkbox',
+    props: {
+      title: 'Is the current person a minor',
+      default: false,
+    },
+  },
+  {
+    token: 'PERSON_DOB',
+    type: 'date',
+    props: {
+      title: 'Enter your DOB',
+      placeholder: 'e.g 01/01/2000',
+      required:"IS_PERSON_MINOR"
+    },
+  },
+]);
+
+const formData = ref({});
+const handleSubmit = () => {
+  console.log('Form submitted:', formData.value);
+};
 </script>
